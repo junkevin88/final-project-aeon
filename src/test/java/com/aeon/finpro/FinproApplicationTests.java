999218664817caf80dd7952274cbd7ada783b80b
junkevin88 <junkevin88@gmail.com>
2023-03-27 19:11:21 +0700

    add template

---
+++ src/test/java/com/aeon/finpro/FinproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeon.finpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
